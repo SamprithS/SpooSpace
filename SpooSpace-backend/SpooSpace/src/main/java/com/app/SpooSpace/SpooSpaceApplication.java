ed8daeeac4d489eaf95d40aef44a673367829c4b
Samprith S <samprith052003@gmail.com>
2026-04-05 15:55:50 +0530

    Created the backend and the frontend folder with the basic requirements

---
+++ SpooSpace-backend/SpooSpace/src/main/java/com/app/SpooSpace/SpooSpaceApplication.java
@@ -0,0 +1,12 @@
+package com.app.SpooSpace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpooSpaceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpooSpaceApplication.class, args);
+	}
+
+}
---
+++ SpooSpace-backend/SpooSpace/src/test/java/com/app/SpooSpace/SpooSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.SpooSpace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpooSpaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
