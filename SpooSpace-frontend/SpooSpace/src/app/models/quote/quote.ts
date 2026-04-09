856d9d32585b25d7730e262d61182127149c14b1
Samprith S <samprith052003@gmail.com>
2026-04-09 21:06:12 +0530

    Cleanup: Removing broken frontend to start fresh

--- SpooSpace-frontend/SpooSpace/src/app/models/quote/quote.ts
+++
@@ -1,9 +0,0 @@
-// This is a TYPE. It cannot be used in 'declarations' or 'imports' arrays.
-export interface Quote {
-  id: number;
-  quoteText: string;
-  memberName: string;
-  groupName: string;
-  mood: 'COMFORT' | 'CONFIDENCE' | 'SOFT' | 'HAPPY' | 'INSPIRATION';
-  imageUrl: string;
-}
