3ebbec8b06064bfc7a4248bdbc906fae06f8fb6b
Samprith S <samprith052003@gmail.com>
2026-04-10 23:31:57 +0530

    Nothing

--- SpooSpace-frontend/SpooSpace/src/app/components/star-field/star-field.component.spec.ts
+++ SpooSpace-frontend/SpooSpace/src/app/components/star-field/star-field.component.spec.ts
@@ -1,17 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { StarField } from './star-field.component';
+import { StarFieldComponent } from './star-field.component';
 
-describe('StarField', () => {
-  let component: StarField;
-  let fixture: ComponentFixture<StarField>;
+describe('StarFieldComponent', () => {
+  let component: StarFieldComponent;
+  let fixture: ComponentFixture<StarFieldComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [StarField],
+      imports: [StarFieldComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(StarField);
+    fixture = TestBed.createComponent(StarFieldComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
